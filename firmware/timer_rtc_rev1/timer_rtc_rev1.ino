41625b8422364396853c1c49cd7003316a42dd48
Michael Harris <mharris@visgence.com>
2013-07-23 21:03:04 -0600

    added firmware

---
+++ firmware/timer_rtc_rev1/timer_rtc_rev1.ino
@@ -0,0 +1,38 @@
+
+#define RELAY A3
+#define BACKLIGHT A4 
+//TODO #define UP
+//TODO #define LEFT
+//TODO #define RIGHT
+//TODO #define SELECT
+//TODO #define TRIGGER
+//TODO #define OUT
+//TODO #define ADC
+
+void setup() {
+  pinMode(RELAY, OUTPUT);
+  pinMode(BACKLIGHT, OUTPUT);
+  
+  Serial.begin(9600);
+  
+  //This would actually never be seen, best to print it on the display
+  //Serial.println("Hello, world, this is the Chronomancer (rev 1)!");
+  
+  digitalWrite(RELAY, HIGH);
+  digitalWrite(BACKLIGHT, HIGH);
+  delay(1000);
+  digitalWrite(RELAY, LOW);
+  digitalWrite(BACKLIGHT, LOW);
+  delay(1000);
+}
+
+//a simple loop to test serial comms
+char i = 33;
+void loop() {
+  if(i > 126)
+    i = 33;
+  
+  Serial.println(i);
+  i++;
+  delay(100);
+}
