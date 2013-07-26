deb439a530116ac7b07bbe6c6f62d44ffb6a7edb
Michael Harris <mharris@visgence.com>
2013-07-25 22:00:51 -0600

    adds proof-of-concept RTC code

--- firmware/timer_rtc_rev1/timer_rtc_rev1.ino
+++ firmware/timer_rtc_rev1/timer_rtc_rev1.ino
@@ -1,6 +1,14 @@
+/*
+* timer_rtc_rev1
+* "Chronomancer" 
+*
+* (c) 2013 Visgence, Inc.
+*
+*/
 
-#define RELAY A3
-#define BACKLIGHT A4 
+#include <Wire.h>
+#include "RTClib.h"
+
 //TODO #define UP
 //TODO #define LEFT
 //TODO #define RIGHT
@@ -9,7 +17,11 @@
 //TODO #define OUT
 //TODO #define ADC
 
-#define RTC 0xD0 //I2C address of clock
+RTC_DS1307 rtc;
+DateTime date;
+
+#define RELAY A3
+#define BACKLIGHT A4 
 
 void setup() {
   pinMode(RELAY, OUTPUT);
@@ -20,21 +32,32 @@ void setup() {
   //This would actually never be seen, best to print it on the display
   //Serial.println("Hello, world, this is the Chronomancer (rev 1)!");
   
+  //Test relay and backlight
   digitalWrite(RELAY, HIGH);
   digitalWrite(BACKLIGHT, HIGH);
   delay(1000);
   digitalWrite(RELAY, LOW);
   digitalWrite(BACKLIGHT, LOW);
   delay(1000);
+  
+  //initialize and test RTC
+  Wire.begin();
+  rtc.begin();
+  rtc.adjust(DateTime(__DATE__,__TIME__));
+  date = rtc.now();
 }
 
 //a simple loop to test serial comms
 char i = 33;
 void loop() {
+  date = rtc.now();
+  
   if(i > 126)
     i = 33;
   
   Serial.println(i);
+  Serial.println(date.unixtime());
   i++;
   delay(100);
+  
 }
